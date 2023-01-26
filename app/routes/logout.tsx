1b24645afa86a1e82bc9aeac6043a1c3fc2a3c00
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-01-26 17:30:01 -0500

    feat: add dummy page

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -3,10 +3,10 @@ import { redirect } from "@remix-run/node";
 
 import { logout } from "~/session.server";
 
-export async function action({ request }: ActionArgs) {
+export function action({ request }: ActionArgs) {
   return logout(request);
 }
 
-export async function loader() {
+export function loader() {
   return redirect("/");
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: colors.indigo,
+        ...colors,
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
