0037511743b847103ddb8dcf1292663f4983ec47
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-01-27 15:53:57 -0500

    feat: add back alert and fix styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,10 +4,10 @@ import { json } from "@remix-run/server-runtime";
 import { withZod } from "@remix-validated-form/with-zod";
 import { z } from "zod";
 import { ValidatedForm, validationError } from "remix-validated-form";
-// import { Alert } from "~/design-system/Alert";
 import { zfd } from "zod-form-data";
 import { Field, FieldInput } from "~/ui/form/Field";
 import { SubmitButton } from "~/ui/form/SubmitButton";
+import { Alert } from "~/ui/Alert";
 
 const validator = withZod(
   z.object({
@@ -31,38 +31,36 @@ export default function Example() {
   const data = useActionData<typeof action>();
   return (
     <>
-      <div className="flex min-h-full">
-        <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div className="mt-8">
-              {/* {data && "message" in data && (
-                <Alert variant="info" details={data.message} />
-              )} */}
-              <div className="mt-6">
-                <ValidatedForm
-                  validator={validator}
-                  action="?index"
-                  method="post"
-                  className="space-y-6"
+      <div className="flex min-h-full flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="mt-8">
+            {data && "message" in data && (
+              <Alert variant="info" details={data.message} />
+            )}
+            <div className="mt-6">
+              <ValidatedForm
+                validator={validator}
+                action="?index"
+                method="post"
+                className="space-y-6"
+              >
+                <Field
+                  name="name"
+                  label="Name"
+                  description="Should say your name"
                 >
-                  <Field
-                    name="name"
-                    label="Name"
-                    description="Should say your name"
-                  >
-                    <FieldInput />
-                  </Field>
-                  <Field name="email" label="Email">
-                    <FieldInput />
-                  </Field>
-                  <SubmitButton
-                    variant="default"
-                    label="Submit"
-                    loadingLabel="Submitting..."
-                    className="w-full"
-                  />
-                </ValidatedForm>
-              </div>
+                  <FieldInput />
+                </Field>
+                <Field name="email" label="Email">
+                  <FieldInput />
+                </Field>
+                <SubmitButton
+                  variant="default"
+                  label="Submit"
+                  loadingLabel="Submitting..."
+                  className="w-full"
+                />
+              </ValidatedForm>
             </div>
           </div>
         </div>
