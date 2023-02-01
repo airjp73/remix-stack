81b5903372499efbc857731794667ea51d416cfb
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-01-31 23:25:56 -0500

    tweak: add dark mode alert styling

--- app/ui/Alert.tsx
+++ app/ui/Alert.tsx
@@ -32,9 +32,12 @@ export const Alert: FC<AlertProps> = ({
       role="alert"
       className={classNames(
         "rounded-md border border-red-400 p-4",
-        variant === "error" && "border-red-400 bg-red-50",
-        variant === "info" && "border-blue-400 bg-blue-50",
-        variant === "success" && "border-green-400 bg-green-50",
+        variant === "error" &&
+          "dark:bg-border-red-200 border-red-400 bg-red-50 dark:bg-red-900",
+        variant === "info" &&
+          "dark:bg-border-blue-200 dar:bg-blue-900 border-blue-400 bg-blue-50",
+        variant === "success" &&
+          "dark:bg-border-green-200 border-green-400 bg-green-50 dark:bg-green-900",
         className
       )}
     >
@@ -43,9 +46,9 @@ export const Alert: FC<AlertProps> = ({
           <Icon
             className={classNames(
               "h-5 w-5",
-              variant === "error" && "text-red-500",
-              variant === "info" && "text-blue-500",
-              variant === "success" && "text-green-500"
+              variant === "error" && "text-red-500 dark:text-red-400",
+              variant === "info" && "text-blue-500 dark:text-blue-400",
+              variant === "success" && "text-green-500 dark:text-green-400"
             )}
           />
         </div>
@@ -54,9 +57,9 @@ export const Alert: FC<AlertProps> = ({
             <h3
               className={classNames(
                 "text-sm font-medium",
-                variant === "error" && "text-red-800",
-                variant === "info" && "text-blue-800",
-                variant === "success" && "text-green-800"
+                variant === "error" && "text-red-800 dark:text-red-200",
+                variant === "info" && "text-blue-800 dark:text-blue-200",
+                variant === "success" && "text-green-800 dark:text-green-200"
               )}
             >
               {title}
@@ -67,9 +70,9 @@ export const Alert: FC<AlertProps> = ({
               className={classNames(
                 "text-sm",
                 !!title && "mt-2",
-                variant === "error" && "text-red-700",
-                variant === "info" && "text-blue-700",
-                variant === "success" && "text-green-700"
+                variant === "error" && "text-red-700 dark:text-red-300",
+                variant === "info" && "text-blue-700 dark:text-blue-300",
+                variant === "success" && "text-green-700 dark:text-green-300"
               )}
             >
               {details}
