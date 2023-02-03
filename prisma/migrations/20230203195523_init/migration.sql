9c0a48e9eece8f80c45268f6d1f0f29ef401cecb
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-02-03 14:57:14 -0500

    feat: add profile photo

--- prisma/migrations/20230203195523_init/migration.sql
+++ prisma/migrations/20230203195523_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "firebase_uid" TEXT NOT NULL,
+    "profile_photo" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
