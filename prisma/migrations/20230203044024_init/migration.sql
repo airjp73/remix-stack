dc39afa9d5f820f0fc655f0c085440c86c905d04
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-02-02 23:40:42 -0500

    fix: update prisma migration to new schema

---
+++ prisma/migrations/20230203044024_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firebase_uid" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firebase_uid_key" ON "User"("firebase_uid");
