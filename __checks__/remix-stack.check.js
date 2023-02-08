fc2d0933cfc40cbd849a28a5dd1689ba56ef96fb
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-02-08 10:54:29 -0500

    fix: check for env variables to get better error message

--- __checks__/remix-stack.check.js
+++ __checks__/remix-stack.check.js
@@ -6,6 +6,11 @@
 const { expect, test } = require("@playwright/test");
 
 test("visit page and take screenshot", async ({ page }) => {
+  const testEmail = process.env.TEST_EMAIL;
+  const testPassword = process.env.TEST_PASSWORD;
+  if (!testEmail) throw new Error("TEST_EMAIL is not set");
+  if (!testPassword) throw new Error("TEST_PASSWORD is not set");
+
   // If available, we set the target URL to a preview deployment URL provided by the ENVIRONMENT_URL created by Vercel.
   // Otherwise, we use the Production URL.
   const targetUrl =
@@ -22,9 +27,9 @@ test("visit page and take screenshot", async ({ page }) => {
 
   const email = page.getByRole("textbox", { name: /email/i });
   await email.waitFor();
-  await email.type(process.env.TEST_EMAIL);
+  await email.type(testEmail);
 
-  await page.getByLabel(/password/i).type(process.env.TEST_PASSWORD);
+  await page.getByLabel(/password/i).type(testPassword);
 
   await page.getByRole("button", { name: /login/i }).click();
   await page.getByText(/dashboard/i).waitFor();
