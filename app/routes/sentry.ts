64b8b3d92e80aadee136b7cf32ad7191018c8c84
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-02-07 23:34:42 -0500

    fix: include a GET in sentry tunnel

--- app/routes/sentry.ts
+++ app/routes/sentry.ts
@@ -1,22 +1,19 @@
-import { ActionArgs, json } from "@remix-run/server-runtime";
+import { DataFunctionArgs } from "@remix-run/server-runtime";
 
 const sentryHost = "sentry.io";
 
-export const action = async ({ request }: ActionArgs) => {
+const handler = async ({ request }: DataFunctionArgs) => {
   const envelope = await request.text();
   const pieces = envelope.split("\n");
   const header = JSON.parse(pieces[0]);
-  const { host, pathname } = new URL(header.dsn);
+  const { pathname } = new URL(header.dsn);
 
   // remove leading slash
   const projectId = pathname.substring(1);
 
-  // if (host !== sentryHost) {
-  //   throw new Error(`Invalid Sentry host: ${host}`);
-  // }
-
   const ingestUrl = `https://${sentryHost}/api/${projectId}/envelope/`;
-  const response = await fetch(ingestUrl, { method: "POST", body: envelope });
-
-  return json({ status: response.status }, { status: response.status });
+  return await fetch(ingestUrl, { method: "POST", body: envelope });
 };
+
+export const action = handler;
+export const loader = handler;
