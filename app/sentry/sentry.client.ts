274c9cf0b835029b119d7a0800e923f2c03b0867
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-02-08 13:53:10 -0500

    chore: ignore hydration errors with sentry

--- app/sentry/sentry.client.ts
+++ app/sentry/sentry.client.ts
@@ -10,6 +10,12 @@ export const initClientSentry = () => {
     dsn: clientEnv.SENTRY_DSN,
     tunnel: "/sentry",
     tracesSampleRate: 1,
+    ignoreErrors: [
+      "https://reactjs.org/docs/error-decoder.html?invariant=418", // Hydration failed because the initial UI does not match what was rendered on the server.
+      "https://reactjs.org/docs/error-decoder.html?invariant=422", // There was an error while hydrating this Suspense boundary. Switched to client rendering.
+      "https://reactjs.org/docs/error-decoder.html?invariant=423", // There was an error while hydrating.
+      "https://reactjs.org/docs/error-decoder.html?invariant=425", // Text content does not match server-rendered HTML...
+    ],
     integrations: [
       new Sentry.BrowserTracing({
         routingInstrumentation: Sentry.remixRouterInstrumentation(
